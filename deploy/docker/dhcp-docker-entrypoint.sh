ba2ae22a029aadbe233d972c809208e536f1be9e
Dieter <dieterbocklandt@gmail.com>
2021-11-21 23:38:28 +0100

    WIP

---
+++ deploy/docker/dhcp-docker-entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+DOCKER_IP=$(ip addr show dev eth0 | grep 'inet\b' | awk '{print $2}')
+DOCKER_NETWORK=$(ipcalc -n --no-decorate $DOCKER_IP)
+DOCKER_MASK=$(ipcalc -m --no-decorate $DOCKER_IP)
+
+if test -f /etc/dhcp/dhcpd.local.conf; then 
+    cat /etc/dhcp/dhcpd.local.conf >> /etc/dhcp/dhcpd.conf
+fi
+
+echo """
+# Needed so dhcpd will start
+# We'll use a DHCP relay to get DHCP traffic to the Docker container
+subnet $DOCKER_NETWORK netmask $DOCKER_MASK {
+}
+""" >> /etc/dhcp/dhcpd.conf
+
+exec "$@"
